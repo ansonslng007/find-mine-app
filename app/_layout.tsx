0ade684eecde28fa32704edf8a0aae1365735d4c
AnsonNg <ansonsiulung@gmail.com>
2026-05-05 00:21:20 +0800

    style: animation slide"

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,17 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-import { AppQueryProvider } from '@/providers/query-client-provider';
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import { AppQueryProvider } from "@/providers/query-client-provider";
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+  anchor: "(tabs)",
 };
 
 export default function RootLayout() {
@@ -15,18 +19,21 @@ export default function RootLayout() {
 
   return (
     <AppQueryProvider>
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen
             name="item/[id]"
             options={{
               headerShown: false,
-              presentation: 'modal',
-              animation: 'slide_from_bottom',
+              presentation: "card",
+              animation: "slide_from_right",
             }}
           />
-          <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+          <Stack.Screen
+            name="modal"
+            options={{ presentation: "modal", title: "Modal" }}
+          />
         </Stack>
         <StatusBar style="auto" />
       </ThemeProvider>
